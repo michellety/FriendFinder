5606eb9145c2408d0d033bf6b70857b05d5febc4
michelle <michelletyson29@gmail.com>
2019-03-29 09:13:53 -0700

    added form, module exports

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,13 +5,22 @@ var friendData = require("../data/friends");
 //a GET route with the url /api/friends
 //display a JSON of all possible friends
 
-app.get("/api/friends", function(req, res) {
-    res.json(friendData);
-}); 
+module.exports = function (app) {
 
-//a POST route /api/friends
-//handle incoming survey results
+    app.get("/api/friends", function (req, res) {
+        res.json(friendData);
+    });
 
-app.post("/api/friends", function(req, res) {
-    
-})
+    //a POST route /api/friends
+    //handle incoming survey results
+
+    app.post("/api/friends", function (req, res) {
+        console.log(req.body)
+        res.json(friendData)
+        //comparison loops here
+        //Math.abs shows absolute value 
+        
+
+    })
+
+}
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,18 @@
 //two routes
+//include the path package to get the correct file path for the html
+var path = require("path");
+
+//allowing server to have access to this function
+module.exports = function(app) {
 
 //a GET route /survey, display the survey page at survey.html
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  });
 
 //default, displays the home page at home.html
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
 
+}
