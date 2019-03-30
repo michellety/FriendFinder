eca37888390e3ed4db35ab536c2f135073a0d3ed
michelle <michelletyson29@gmail.com>
2019-03-29 17:06:04 -0700

    comparisons

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,12 +4,12 @@ var friendArray = [
     {
         name : "A",
         photo : "https://www.pinterest.com/pin/147985537736877990/",
-        scores : [2, 3, 1 ,4 , 5]
+        scores : [5, 4, 5, 1, 2, 3, 1, 4, 5, 2]
     },
     {
         name : "B",
         photo : "https://www.pinterest.com/pin/AXw24dFnIUTfNuQcjJFxYR2o0sd5yyNeH74DyU81Zcs162ZYEh5BEX0/",
-        scores : [1, 1, 3, 4, 2]
+        scores : [1, 1, 1, 2, 5, 4, 2, 3, 4, 2]
     }
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,7 @@ var friendData = require("../data/friends");
 //display a JSON of all possible friends
 
 module.exports = function (app) {
-
+    //gets data from friends.js
     app.get("/api/friends", function (req, res) {
         res.json(friendData);
     });
@@ -15,12 +15,55 @@ module.exports = function (app) {
     //handle incoming survey results
 
     app.post("/api/friends", function (req, res) {
-        console.log(req.body)
-        res.json(friendData)
+        //save the scores
+        var userScore = req.body;
+        var newUser = {
+            name: userScore.name,
+            photo: userScore.photo,
+            scores: []
+        };
+        console.log("new friend data from user", newUser);
+
+        //change the scores into integers, then save to the newUser 
+        var scoreArr = [];
+        for (var i = 0; i < userScore.length; i++) {
+            scoreArr.push(parseInt(userScore[i]))
+        }
+        newUser.scores = scoreArr;
+
         //comparison loops here
         //Math.abs shows absolute value 
-        
+        //array to hold the comparison score
+        var comparisonArr = [];
+        //compare to friend entries 
+        for (var i = 0; i < friendData.length; i++) {
+            var diff = 0;
+            for (var j = 0; j < newUser.scores.length; j++) {
+                diff += Math.abs(newUser.scores[j] - friendData[i].scores[j]);
+            }
+            //add the comparison differences into the array
+            comparisonArr.push(diff);
+        }
 
+        //determine the closest match
+        var bf = 0;
+        for (var i = 1; i < comparisonArr.length; i++) {
+            //lowest number is the closest match
+            if (comparisonArr[i] <= comparisonArr[bf]) {
+                bf = i;
+            }
+        }
+        console.log("best friend match", bf);
+        var match = {};
+        match = friendData[bf];
+        //response with the index of the best match
+        
+        //add user to the friendData array
+        friendData.push(newUser);
+        console.log("user data saved");
+    res.json(match);
     })
 
 }
+
+
